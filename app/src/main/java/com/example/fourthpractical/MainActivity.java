2d3177edacef818df4ff3ad06c9b25c8f50dd78f
ivissivi <ivis31121999@gmail.com>
2021-12-29 13:30:22 +0200

    Added buttons

--- app/src/main/java/com/example/fourthpractical/MainActivity.java
+++ app/src/main/java/com/example/fourthpractical/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.fourthpractical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn0, btn1, btn2, btn3, btn4, btn5,
+            btn6, btn7, btn8, btn9, btnAdd, btnSubtract,
+            btnDivide, btnMultiply, btn10, btnClear, btnEquals;
+
+    EditText editText;
+
+    float calcValueOne, calcValueTwo;
+
+    boolean calcAdd, calcSubtract, calcMultiply, calcDivide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn10 = (Button) findViewById(R.id.btn10);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btn10 = (Button) findViewById(R.id.btnDivide);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnEquals = (Button) findViewById(R.id.btnEquals);
+        editText = (EditText) findViewById(R.id.editText);
     }
 }
