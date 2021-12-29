5d865b0494579a244665e3d3098195f35a3a86e7
ivissivi <ivis31121999@gmail.com>
2021-12-29 16:14:19 +0200

    Fixed some warnings

--- app/src/main/java/com/example/fourthpractical/MainActivity.java
+++ app/src/main/java/com/example/fourthpractical/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
