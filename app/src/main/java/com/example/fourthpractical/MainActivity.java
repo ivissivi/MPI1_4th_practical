222ae7b1f91acb37f812fb9ecd7abb7d4c101c8e
ivissivi <ivis31121999@gmail.com>
2021-12-29 16:06:26 +0200

    1.0

--- app/src/main/java/com/example/fourthpractical/MainActivity.java
+++ app/src/main/java/com/example/fourthpractical/MainActivity.java
@@ -6,14 +6,15 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn0, btn1, btn2, btn3, btn4, btn5,
             btn6, btn7, btn8, btn9, btnAdd, btnSubtract,
-            btnDivide, btnMultiply, btn10, btnClear, btnEquals;
+            btnDivide, btnMultiply, btnDecimal, btnClear, btnEquals;
 
-    EditText editText;
+    TextView inputField;
 
     float firstValue, secondValue;
 
@@ -35,93 +36,92 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.btn7);
         btn8 = findViewById(R.id.btn8);
         btn9 = findViewById(R.id.btn9);
-        btn10 = findViewById(R.id.btn10);
+        btnDecimal = findViewById(R.id.btnDecimal);
         btnAdd = findViewById(R.id.btnAdd);
         btnSubtract = findViewById(R.id.btnSubtract);
         btnMultiply = findViewById(R.id.btnMultiply);
         btnDivide = findViewById(R.id.btnDivide);
         btnClear = findViewById(R.id.btnClear);
         btnEquals = findViewById(R.id.btnEquals);
-        editText = findViewById(R.id.textEdit);
+        inputField = findViewById(R.id.txtView);
 
+        btn1.setOnClickListener(v -> inputField.setText(inputField.getText() + "1"));
 
-        btn1.setOnClickListener(v -> editText.setText(editText.getText() + "1"));
+        btn2.setOnClickListener(v -> inputField.setText(inputField.getText() + "2"));
 
-        btn2.setOnClickListener(v -> editText.setText(editText.getText() + "2"));
+        btn3.setOnClickListener(v -> inputField.setText(inputField.getText() + "3"));
 
-        btn3.setOnClickListener(v -> editText.setText(editText.getText() + "3"));
+        btn4.setOnClickListener(v -> inputField.setText(inputField.getText() + "4"));
 
-        btn4.setOnClickListener(v -> editText.setText(editText.getText() + "4"));
+        btn5.setOnClickListener(v -> inputField.setText(inputField.getText() + "5"));
 
-        btn5.setOnClickListener(v -> editText.setText(editText.getText() + "5"));
+        btn6.setOnClickListener(v -> inputField.setText(inputField.getText() + "6"));
 
-        btn6.setOnClickListener(v -> editText.setText(editText.getText() + "6"));
+        btn7.setOnClickListener(v -> inputField.setText(inputField.getText() + "7"));
 
-        btn7.setOnClickListener(v -> editText.setText(editText.getText() + "7"));
+        btn8.setOnClickListener(v -> inputField.setText(inputField.getText() + "8"));
 
-        btn8.setOnClickListener(v -> editText.setText(editText.getText() + "8"));
+        btn9.setOnClickListener(v -> inputField.setText(inputField.getText() + "9"));
 
-        btn9.setOnClickListener(v -> editText.setText(editText.getText() + "9"));
-
-        btn0.setOnClickListener(v -> editText.setText(editText.getText() + "0"));
+        btn0.setOnClickListener(v -> inputField.setText(inputField.getText() + "0"));
 
         btnAdd.setOnClickListener(v -> {
 
-            if (editText == null) {
+            if (inputField == null) {
                 assert false;
-                editText.setText("");
+                inputField.setText("");
             } else {
-                firstValue = Float.parseFloat(editText.getText() + "");
+                firstValue = Float.parseFloat(inputField.getText() + "");
                 calcAdd = true;
-                editText.setText(null);
+                inputField.setText(null);
             }
         });
 
         btnSubtract.setOnClickListener(v -> {
-            firstValue = Float.parseFloat(editText.getText() + "");
+            firstValue = Float.parseFloat(inputField.getText() + "");
             calcSubtract = true;
-            editText.setText(null);
+            inputField.setText(null);
         });
 
         btnMultiply.setOnClickListener(v -> {
-            firstValue = Float.parseFloat(editText.getText() + "");
+            firstValue = Float.parseFloat(inputField.getText() + "");
             calcMultiply = true;
-            editText.setText(null);
+            inputField.setText(null);
         });
 
         btnDivide.setOnClickListener(v -> {
-            firstValue = Float.parseFloat(editText.getText() + "");
+            firstValue = Float.parseFloat(inputField.getText() + "");
             calcDivide = true;
-            editText.setText(null);
+            inputField.setText(null);
         });
 
         btnEquals.setOnClickListener(v -> {
-            secondValue = Float.parseFloat(editText.getText() + "");
+            secondValue = Float.parseFloat(inputField.getText() + "");
 
             if (calcAdd) {
-                editText.setText(firstValue + secondValue + "");
+                inputField.setText(firstValue + secondValue + "");
                 calcAdd = false;
             }
 
             if (calcSubtract) {
-                editText.setText(firstValue - secondValue + "");
+                inputField.setText(firstValue - secondValue + "");
                 calcSubtract = false;
             }
 
             if (calcMultiply) {
-                editText.setText(firstValue * secondValue + "");
+                inputField.setText(firstValue * secondValue + "");
                 calcMultiply = false;
             }
 
             if (calcDivide) {
-                editText.setText(firstValue / secondValue + "");
+                inputField.setText(firstValue / secondValue + "");
                 calcDivide = false;
             }
         });
 
-        btnClear.setOnClickListener(v -> editText.setText(""));
+        btnClear.setOnClickListener(v -> inputField.setText(""));
 
-        btn10.setOnClickListener(v -> editText.setText(editText.getText() + "."));
+        btnDecimal.setOnClickListener(v -> inputField.setText(inputField.getText() + "."));
     }
 }
 
